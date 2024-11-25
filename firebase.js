37afa3200aeb1d8e00f03139e66b00fdfb1f723b
codespirityash <codespirityash@gmail.com>
2024-11-25 20:43:32 +0530

    Initialize Firebase

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZoRIUSLIzIrMPCmcPgtJ5BDOpetxId9c",
+    authDomain: "vite-vanilla-portfolio.firebaseapp.com",
+    projectId: "vite-vanilla-portfolio",
+    storageBucket: "vite-vanilla-portfolio.firebasestorage.app",
+    messagingSenderId: "1064851362471",
+    appId: "1:1064851362471:web:57a7e7ff2c03c36bed0182"
+  };
+  
+  const app = initializeApp(firebaseConfig);
