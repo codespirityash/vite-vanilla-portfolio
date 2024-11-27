56e02454853fd7ff2699db4e09adc401d5975f50
yashweblife <yashweblife20@gmail.com>
2024-11-26 18:35:29 -0800

    remove firebase file and add config to controller

--- src/lib/FirebaseManager/index.js
+++ src/lib/FirebaseManager/index.js
@@ -1,4 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_AUTH,
+    authDomain: "vite-vanilla-portfolio.firebaseapp.com",
+    projectId: "vite-vanilla-portfolio",
+    storageBucket: "vite-vanilla-portfolio.firebasestorage.app",
+    messagingSenderId: "1064851362471",
+    appId: "1:1064851362471:web:57a7e7ff2c03c36bed0182"
+  };
+  
 export class FirebaseManager {
+    app = initializeApp(firebaseConfig);
     constructor() {}
     getFromDatabase(){}
 }
