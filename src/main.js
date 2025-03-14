26f2d46a3c20bc6b435a59d0741a230f3df08952
yashweblife <yashweblife20@gmail.com>
2025-03-14 10:20:43 -0500

    properly import safer code

--- src/main.js
+++ src/main.js
@@ -1,42 +1,8 @@
+import Carousel from "./lib/Carousel";
+import DropDown from "./lib/DropDown";
+import Menu from "./lib/Menu";
 import "./styles/main.scss";
-// import "contacts.js";
-// import "projects.js";
-import "./nav.js";
 
-
-//  Carousel
-let currentProject = 0;
-const projectCards = document.querySelectorAll(".project-card");
-
-function showProject(index) {
-  projectCards.forEach((card, i) => {
-    card.classList.toggle("active", i === index);
-  });
-}
-
-function nextProject() {
-  currentProject = (currentProject + 1) % projectCards.length;
-  showProject(currentProject);
-}
-
-function prevProject() {
-  currentProject =
-    (currentProject - 1 + projectCards.length) % projectCards.length;
-  showProject(currentProject);
-}
-
-//Skills Dropdown
-function toggleDropdown(button) {
-  const dropdown = button.nextElementSibling;
-  dropdown.classList.toggle("active");
-  const icon = button.querySelector("i");
-  icon.classList.toggle("fa-chevron-down");
-  icon.classList.toggle("fa-chevron-up");
-}
-
-document.querySelector(".left-arrow").addEventListener("click", prevProject);
-document.querySelector(".right-arrow").addEventListener("click", nextProject);
-
-document.querySelectorAll(".skill-title").forEach((button) => {
-  button.addEventListener("click", () => toggleDropdown(button));
-});
+const carousel = new Carousel();
+const dropDown = new DropDown();
+const menu = new Menu();
